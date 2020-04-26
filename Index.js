3eb9094cfb02e16b881c8279d1b869a8ef72b9a8
William Francois <william.francois@outlook.com>
2020-04-26 16:48:58 +0200

    Spinner

--- Index.js
+++ Index.js
@@ -5,3 +5,17 @@ return new Promise((resolve, reject) => {
 },3000);
 
 }
+
+
+
+$( "#button" ).click(function() {
+    alert("test");
+    // document.querySelector(".spin").style.display ="none";
+console.log($( "#spin" ))
+var x = document.getElementById("spin");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+  });
